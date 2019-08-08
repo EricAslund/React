ff1dfdb8a63e6cd8c84e564c93c79fc05c5f5c74
Eric Christian Radicchi Aslund <eric.aslund@ntendencia.com.br>
2019-08-08 17:52:48 -0300

    .

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,43 +3,48 @@ import { Grid } from 'semantic-ui-react';
 import api from '../../api';
 import Add from '../..//components/addRepositories';
 import Repositorios from '../../components/repositories';
+import './style.css';
 
 
 class Main extends Component {
 
-    state = {
-        listRepos: [{
-            id: 1,
-            nome: "lucas"
-        },
-        {
-            id: 2,
-            nome: "lucas"
-        }]
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: '',
+            listRepos: []
+        }
     }
 
-    async addRepository(event) {
-        event.preventDefault();
-        console.log(event);
-        const repoInput = event.value;
+    addRepository = async valRepo => {
+        try {
+            const reponse = await api.get(`/repos/${valRepo}`);
 
-        if (repoInput === null)
-            return;
-        console.log(this.state.listRepos)
-        this.setState({ listRepos: [...this.state.listRepos] })
+            const { id, name, owner: { login }, owner: { avatar_url }, stargazers_count, language, forks_count } = reponse.data;
+
+
+            this.setState({
+                listRepos: [...this.state.listRepos, reponse.data]
+            });
+        } catch (err) {
+            alert('O repositorio não existe!');
+        }
+
+
     }
 
     render() {
         return (
             <Fragment>
-                <Add find={this.addRepository} />
-                <Grid columns={6} divided>
+                <Add addRepository={this.addRepository} />
+                <div className="flex-container" >
                     {
                         this.state.listRepos.map(repo => (
                             <Repositorios key={repo.id} repositorio={repo} />
                         ))
                     }
-                </Grid>
+                </div>
             </Fragment>
         );
     }
